871e5665ee21044a0cf02e0d599aff8eedc56ea2
Chris Evans <chris.evans@astrion.us>
2025-12-23 15:26:24 -0600

    added notes

--- AzureTofu/myAzureDev.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-
-docker run -it    -v /home/ubuntu/.ssh:/home/ubuntu/.ssh -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/working  -p 22:2222 -p 443:8443  -w $(pwd) --rm --name cae-azure-cli --hostname cae-azure-dev my-azure-development-image:latest bash
-
-        #--env-file=$(read -p "Enter environment file path: ") \
